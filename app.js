859fa7c06bf7ed1f543ff06e341f19fd57d7cc0d
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-25 15:47:20 +0000

    understood loops a lot more with playing around with them

--- app.js
+++ app.js
@@ -9,14 +9,26 @@ while (counter <= 5) {
 }
 
 let keepLooping = true;
-let ii = 0;
+let i = 0;
 while (keepLooping === true) {
-  console.log("IIteration", ii);
-  keepLooping = Math.random() < 0.1;
-  ii++;
+  console.log("Iteration", i);
+  keepLooping = Math.random() < 0.5;
+  i++;
 }
 
 const foods = ["pizza", "chocolate", "ice cream", "cheese"];
-for (let iii = 0; iii < foods.length; iii++) {
-  console.log(foods[1]);
+for (let i = 0; i < foods.length; i++) {
+  console.log(foods[i]);
 }
+
+for (let food of foods) {
+  console.log(food);
+}
+
+foods.forEach(function (food) {
+  console.log(food);
+});
+
+foods.forEach(function (food, index) {
+  console.log(index, food);
+});
